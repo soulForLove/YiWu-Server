72566cc42b70f05ad0cc651f10dfb0e9b8f9d0e5
Lin <linzhongtai@gengee.cn>
2017-08-23 18:16:55 +0800

    【YiWu-Server】方法添加注釋
    (cherry picked from commit 5687c20)

--- ApiCenter/src/main/java/com/yiwu/changething/sec1/mapper/OrderMapper.java
+++ ApiCenter/src/main/java/com/yiwu/changething/sec1/mapper/OrderMapper.java
@@ -5,22 +5,90 @@ import com.yiwu.changething.sec1.enums.OrderStatusType;
 import com.yiwu.changething.sec1.bean.OrderBean;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
  * Created by LinZhongtai <linzhongtai@gengee.cn>
  */
 public interface OrderMapper {
-
+    /**
+     * 根据id获取订单
+     *
+     * @param orderId
+     * @return
+     */
     OrderBean getOrderById(@Param("orderId") String orderId);
 
+    /**
+     * 新增訂單
+     *
+     * @param orderBean
+     */
     void insert(OrderModel orderBean);
 
+    /**
+     * 更新訂單
+     *
+     * @param orderBean
+     */
     void update(OrderModel orderBean);
 
+    /**
+     * 根據id刪除訂單
+     *
+     * @param orderId
+     */
     void deleteById(@Param("orderId") String orderId);
 
+    /**
+     * 更新訂單狀態
+     *
+     * @param orderId
+     * @param status
+     */
     void updateStatus(@Param("orderId") String orderId, @Param("status") OrderStatusType status);
 
+    /**
+     * 批量更新訂單狀態
+     *
+     * @param orderIds
+     * @param status
+     */
+    void updateBatchStatus(@Param("orderIds") List<String> orderIds, @Param("status") OrderStatusType status);
+
+    /**
+     * 付完款的訂單中商品的共享次數
+     *
+     * @param idleId
+     * @return
+     */
     Integer getOrderCountByIdleId(@Param("idleId") String idleId);
 
+    /**
+     * 续费：重新設置周期的次數
+     *
+     * @param orderId
+     * @param cycleNum
+     */
     void renewOrder(@Param("orderId") String orderId, @Param("cycleNum") Integer cycleNum);
+
+    /**
+     * 更新订单时长
+     *
+     * @param orderId
+     * @param duration
+     */
+    void updateDuration(@Param("orderId") String orderId, @Param("duration") Integer duration);
+
+    /**
+     * 每天更新減少時長
+     */
+    void reduceDuration();
+
+    /**
+     * 獲取剩餘時長大於0的訂單列表
+     *
+     * @return
+     */
+    List<OrderBean> getDurationList();
 }
