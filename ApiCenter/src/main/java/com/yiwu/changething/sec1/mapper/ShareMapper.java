5687c2018baf7105009332846907682c295c88e7
Lin <linzhongtai@gengee.cn>
2017-08-23 18:16:55 +0800

    【YiWu-Server】方法添加注釋

--- ApiCenter/src/main/java/com/yiwu/changething/sec1/mapper/ShareMapper.java
+++ ApiCenter/src/main/java/com/yiwu/changething/sec1/mapper/ShareMapper.java
@@ -8,13 +8,34 @@ import org.apache.ibatis.annotations.Param;
  * Created by LinZhongtai <linzhongtai@gengee.cn>
  */
 public interface ShareMapper {
-
+    /**
+     * 新增共享信息
+     *
+     * @param shareModel
+     */
     void insert(ShareModel shareModel);
 
+    /**
+     * 更新共享信息
+     *
+     * @param shareModel
+     */
     void update(ShareModel shareModel);
 
+    /**
+     * 根據id獲取共享信息
+     *
+     * @param shareId
+     * @return
+     */
     ShareBean getShareById(@Param("shareId") String shareId);
 
+    /**
+     * 根據商品id獲取共享信息（一個商品i對應一條共享信息）
+     *
+     * @param idleId
+     * @return
+     */
     ShareBean getShareByIdleId(@Param("idleId") String idleId);
 
 }
